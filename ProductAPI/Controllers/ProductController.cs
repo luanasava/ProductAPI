34541d8320ee1f33074b0afecc2cf9f0796f9a2a
luanasava <luanasava@gmail.com>
2025-03-14 12:47:24 -0300

    Iniciando o projeto

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Models;
+
+namespace ProductAPI.Controllers;
+
+
+[ApiController]
+[Route("produto")]
+public class ProductController : ControllerBase
+{
+
+    private static List<Produto> produtos = new List<Produto>();
+
+    [HttpPost]
+    public void AdicionaProduto([FromBody] Produto produto)
+    {
+        produtos.Add(produto);
+        Console.WriteLine(produto.Modelo);
+        Console.WriteLine(produto.Nome);
+    }
+
+}
---
+++ ProductAPI/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductAPI.Models;
+
+public class Produto
+{
+
+    public string Nome { get; set; }
+
+    public string Modelo { get; set; }
+
+    public int Preco { get; set; }
+}
